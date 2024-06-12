bf2b2b9b64a5a36fbc4378fa616d3c85f26b99f5
Dipsan Kadariya <dipsankadariya99@gmail.com>
2024-06-12 21:06:30 +0545

    new updates

---
+++ datatypes.dart
@@ -0,0 +1,24 @@
+void main() {
+  // Data Types
+  // Numbers, Strings, Booleans, Lists, Maps, Dynamic
+
+  // Strings
+  String firstName = "Dipsan";
+  print("String: $firstName");
+
+  // Integers
+  int myNum = 20;
+  print("Integer: $myNum");
+
+  // Doubles
+  double num = 19.95;
+  print("Double: $num");
+
+  // Booleans
+  bool myBool = true;
+  print("Boolean: $myBool");
+
+  // Dynamic
+  dynamic fullName = "Dipsan kadariya";
+  print('Dynamic: $fullName');
+}
---
+++ variable.dart
@@ -0,0 +1,27 @@
+void main() {
+  //variables in dart
+
+  //use int datatype to store integer value
+  int a = 10;
+  print(a);
+
+  //String
+  String name = "dipsan";
+  print(name);
+
+  //double
+  double value = 10.5;
+  print(value);
+
+  //dynamic
+  dynamic caste = 'kadariya';
+  print(caste);
+
+  //consts
+  const String fullName = 'dipsan kadariya';
+  //finals
+  final String nickName = 'king';
+
+  print(fullName);
+  print(nickName);
+}
